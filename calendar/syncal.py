86f5b64cadfb37895b9e1f3c3b377f0109d5454b
Jason (Linux) <git@jasonpoon.ca>
2010-01-11 13:32:36 -0800

    Altered the debug levels of the syncal script making it less verbose

--- calendar/syncal.py
+++ calendar/syncal.py
@@ -4,7 +4,7 @@ import os
 import logging
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     gcaldaemon = '/opt/gcaldaemon/bin/sync-now.sh'
     
@@ -19,23 +19,25 @@ def main():
     reminder_extension = '.rem'
 
     # Remind -> ics
+    logging.info('remind -> ics')
     for calendar in calendars:
         cal = path_calendar + calendar + calendar_extension
         rem = path_reminders + calendar + reminder_extension
 
-        logging.debug('%s: remind -> ics' % calendar)
+        logging.debug('\t%s' % calendar)
         os.system('remind -s12 %s | %s > %s ' % (rem, rem2ics, cal))
 
     # Sync
-    logging.debug('sync with gcal')
+    logging.info('syncing ical with gcal')
     os.system('%s' % gcaldaemon)
 
     # ics -> Remind
+    logging.info('ics -> remind')
     for calendar in calendars:
         cal = path_calendar + calendar + calendar_extension
         rem = path_reminders + calendar + reminder_extension
 
-        logging.debug('%s: ics -> remind' % calendar)
+        logging.debug('\t%s' % calendar)
         os.system('cat %s | %s --label %s --lead-time 1 > %s' % (cal, ical2rem, calendar, rem))
 
 if __name__ == '__main__':
