e4b2e1c965cda75a27122604095396903dc75f2f
jpoon <git@jasonpoon.ca>
2015-07-18 13:48:49 -0700

    default browser = google-chrome-stable

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -45,7 +45,7 @@ opacity_focus = 1
 opacity_unfocus = 0.85
 
 batteryAdapter = "BAT0"
-browser = "chromium"
+browser = "google-chrome-stable"
 terminal = "urxvt"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
